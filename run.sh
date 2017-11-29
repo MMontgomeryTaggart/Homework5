209de5bbe5caf4350b7d6c374f06550cb28601ad
Max Taggart <max.taggart@gmail.com>
2017-11-29 12:01:23 -0700

    Set up virtualenv in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+virtualenv --system-site-packages ./VENV2.7
+source ./VENV2.7/bin/activate
+
+pip install scikit-learn
+pip install numpy
+
 cd code
 python main.py
+
+cd ..
+
+rm -fr VENV2.7
